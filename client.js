00271e022679680a1c7ed884d8ced70afac6a6d9
FLYBYME <admin@sharefarm.net>
2011-05-03 23:24:02 -0400

    Update

--- client.js
+++ client.js
@@ -7,12 +7,12 @@ var host = args[args.indexOf('--host') + 1];
 var type = args[args.indexOf('--type') + 1];
 var help = args[args.indexOf('--help') + 1];
 
-if(name == 'node') {
-	throw Error('Name is missing try --name bob')
-} else if(port == 'node') {
+if(port == 'node') {
 	throw Error('port is missing try --port 8000')
 } else if(host == 'node') {
 	throw Error('host is missing try --host 192.168.0.100')
+} else if(type == 'node') {
+	throw Error('host is missing try --type websocket OR tcpsocket')
 }
 
-var a = new rpc.client(name ,'socket', host, port)
+var a = new rpc.Client(host, port, type)
