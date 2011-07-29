6891f52b069818d899cdb07088970f26882e330b
Bob <admin@sharefarm.net>
2011-07-29 19:30:38 -0400

    v0.0.1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ console.log(manager)
 //setup the front end server.
 var server = manager.createServer({
 	port : 9999,
-	host : 'localhost',
+	host : '192.168.1.100',
 	type : 'http',
 	root : '/home/bob/Sencha-demo',
 	hasSocket : true
