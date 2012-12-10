713f23adbe3eb20bdef2cb4a6ab8b2b12bc2d36f
flybyme <timprice@mangoraft.com>
2012-12-10 01:50:25 -0500

    Push

--- index.js
+++ index.js
@@ -2,23 +2,9 @@
  *
  *
  */
-module.exports.Stack = require('./lib/stack');
-/*
- *
- *
- */
 module.exports.RpcModule = require('./lib/rpc').RpcModule;
 /*
  *
  *
  */
-module.exports.protocols = {
-	Io : require('./lib/protocols/socket.io'),
-	Process : require('./lib/protocols/process'),
-	Socket : require('./lib/protocols/socket')
-};
-/*
- *
- *
- */
-module.exports.version = '0.0.6';
+module.exports.version = require('./package.json').version;
